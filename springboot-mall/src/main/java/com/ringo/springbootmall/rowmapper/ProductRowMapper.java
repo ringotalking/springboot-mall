4ed0b81ce0c373740aea9610fa0d1c85e6703add
Ringo-NB <sacs1028@gmail.com>
2023-05-02 15:14:17 +0800

    商品功能 - Refactor category，使用 Enum 類型

--- springboot-mall/src/main/java/com/ringo/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/ringo/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.ringo.springbootmall.rowmapper;
 
+import com.ringo.springbootmall.constant.ProductCategory;
 import com.ringo.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,13 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+        //等同下方
+        //product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
