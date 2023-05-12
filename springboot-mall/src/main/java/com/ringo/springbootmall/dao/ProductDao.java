1959473edf9f41b5aeae967da6178c269643e9a3
Ringo-NB <sacs1028@gmail.com>
2023-05-12 18:38:16 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總筆數)

--- springboot-mall/src/main/java/com/ringo/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/ringo/springbootmall/dao/ProductDao.java
@@ -7,6 +7,7 @@ import com.ringo.springbootmall.model.Product;
 import java.util.List;
 
 public interface ProductDao {
+    Integer countProduct(ProductQueryParams productQueryParams);
     List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/ringo/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/ringo/springbootmall/service/ProductService.java
@@ -7,6 +7,8 @@ import com.ringo.springbootmall.model.Product;
 import java.util.List;
 
 public interface ProductService {
+
+    Integer countProduct(ProductQueryParams productQueryParams);
     List<Product> getProducts(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
