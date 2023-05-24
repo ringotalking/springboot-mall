fbc101e1d57400dde924f677ac48dbf722c6ae9a
Ringo-NB <sacs1028@gmail.com>
2023-05-24 19:41:21 +0800

    帳號功能 - 使用MD5加密密碼

--- springboot-mall/src/main/java/com/ringo/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/ringo/springbootmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -24,23 +27,35 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        //檢查 user 是否存在
         if(user == null){
             log.warn("該email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if(user.getPassword().equals(userLoginRequest.getPassword())){
+        //使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if(user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("email的 {} 密碼不正確", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        /*if(user.getPassword().equals(userLoginRequest.getPassword())){
+            return user;
+        }else{
+            log.warn("email的 {} 密碼不正確", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }*/
+
     }
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
-        //檢查註冊的 email
+        // 檢查註冊的 email
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
 
         if(user != null){
@@ -48,7 +63,11 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        //創建帳號
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
